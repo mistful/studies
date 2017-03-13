6eb37365f6ee99525edf7f1783bb787c38fef17e
Aleksey Stoletov <astoletov@names.ru>
2017-03-13 16:57:40 +0300

    stepik python beginners

--- stepik/stepik_py_beginners.py
+++ stepik/stepik_py_beginners.py
@@ -1,17 +1,13 @@
-# n = int(input())
-# for i in range(0, n):
-#     games.add(input().split(';'))
+import collections
 
-with open('games.txt') as g:
-    games = g.readlines()
+with open('dataset_3380_5.txt') as data:
+    classes = [line.strip().split('\t') for line in data.readlines()]
 
-games = [game.strip() for game in games]
-teams = {}
+result = {k: [] for k in range(1, 12)}
 
-for game in games:
-    res = game.split(';')
-    teams[res[0]] = teams.get(res[0], {})
+for cl in classes:
+    result[int(cl[0])].append(int(cl[2]))
 
+result = '\n'.join([str(k) + ' ' + val for k, val in {k: str(sum(val) / len(val)) if len(val) else '-' for k, val in result.items()}.items()])
 
-
-print(teams)
+print(result)
