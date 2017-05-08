b48db283f62283dc02c7562210f734cd445b40c0
Aleksey Stoletov <synnseer@gmail.com>
2017-05-08 20:38:47 +0300

    stepik python 2: inheritance

--- stepik/stepik_py_beginners.py
+++ stepik/stepik_py_beginners.py
@@ -1,10 +1,19 @@
-res = [input().split(' ') for x in range(int(input()))]
+classes = dict()
+for x in range(int(input())):
+    res = input().split(' : ')
+    if len(res) == 1:
+        classes[res[0]] = [res[0]]
+    else:
+        classes[res[0]] = classes.get(res[0], []) + res[1].split(' ')
 
-print(res)
-
-ns = {'global': None}
+for k, v in classes.items():
+    for k1, v1 in classes.items():
+        if k in v1:
+            v1 += v
 
 for x in range(int(input())):
-    command = input().split(' ')
-    if(command[0] == 'add')
-
+    res = input().split(' ')
+    if res[0] in classes[res[1]]:
+        print('Yes')
+    else:
+        print('No')
